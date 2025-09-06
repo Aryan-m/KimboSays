7feccfd56c2308b641afc04f9eb61f5be5a57857
Aryan Maroufkhani <Aryan.Maroufkhani@dss.ca.gov>
2025-09-06 13:59:05 -0700

    - Added Blazored Toast for notifications

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Toast;
 using BlazorUI.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -12,13 +13,11 @@ namespace BlazorUI
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            // Read API URL from configuration
             var apiBaseUrl = builder.Configuration["Api:BaseUrl"];
 
-            // Register HttpClient with the configured base URL
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl!) });
 
-            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddBlazoredToast();
 
             builder.Services.AddScoped<IApiSvc, ApiSvc>();
 
